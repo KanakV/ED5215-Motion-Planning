4d24d2bbd58d0da342d5be8859fe65162c4261bb
Kanak Varma <kanakv004@gmail.com>
2026-02-25 15:40:06 +0530

    Structural changes

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-from atc_grid_env_central import Scenario, AlgoSimulation, MultiAlgorithmVisualizer
+from map.atc_grid_env_central import Scenario, AlgoSimulation, MultiAlgorithmVisualizer
 from planner import centralized_planner
 from planner import centralized_planner
 from planner import centralized_planner
 
+from config.config import SIM_TIME, GRID_SIZE, MAX_PLANES
+
+
+# Maybe add runway position?
+# Creates the Map
+# Has runway and spawn points with time at which the planes spawn
 scenario = Scenario(
-    grid_size=50,
-    max_planes=8,
-    sim_time=60
+    grid_size=GRID_SIZE,
+    max_planes=MAX_PLANES,
+    sim_time=SIM_TIME
 )
 
 simA = AlgoSimulation(scenario, centralized_planner, "Algorithm A")
